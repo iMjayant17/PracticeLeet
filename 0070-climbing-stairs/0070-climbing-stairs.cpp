6f32f2690503ab48c48775d050a8f14e876f18f4
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2023-06-29 03:34:40 +0530

    Time: 0 ms (100%), Space: 6.3 MB (14.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
 
-    int solve(int n,vector<int> &dp){
-        if(n==1) return 1;
-        if(n==2) return 2;
-        if(n==3) return 3;
-        if(dp[n]!=(-1)) return dp[n];
+    // int solve(int n,vector<int> &dp){
+    //     if(n==1) return 1;
+    //     if(n==2) return 2;
+    //     if(n==3) return 3;
+    //     if(dp[n]!=(-1)) return dp[n];
 
-        dp[n] = solve(n-1,dp)+solve(n-2,dp);
+    //     dp[n] = solve(n-1,dp)+solve(n-2,dp);
+    //     return dp[n];
+    //     // memoization
+
+    // }
+
+    int solve2(int n){
+        vector<int> dp(n+5,0);
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i = 4;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
         return dp[n];
-
     }
 
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return solve(n,dp);
+        return solve2(n);
     }
 };
+
+// recursion (exponential) -> memoization (O(n) se thoda  jyada )
+//  -> tabulation (o(n)) -> space optimization(O(n) O(1))
