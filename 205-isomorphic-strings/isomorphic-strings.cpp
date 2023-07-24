3d91c6ec432911d5ccf4957389b39f4e66738156
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2023-07-25 01:14:53 +0530

    Time: 9 ms (50.89%) | Memory: 7.2 MB (11.44%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char,set<char>> m;
-        map<char,set<char>> p;
+        // map<char,set<char>> m;
+        // map<char,set<char>> p;
+        // for(int i = 0;i<s.length();i++){
+        //     m[s[i]].insert(t[i]);
+        //     p[t[i]].insert(s[i]);
+        // }
+
+        // for(auto i : m){
+        //     if(i.second.size()>1){
+        //         return false;
+        //     }
+        // }
+
+        // for(auto i : p){
+        //     if(i.second.size()>1){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+
+        set<char> a[1000];
+        set<char> b[1000];
         for(int i = 0;i<s.length();i++){
-            m[s[i]].insert(t[i]);
-            p[t[i]].insert(s[i]);
+            a[(int)s[i]].insert(t[i]);
+            b[(int)t[i]].insert(s[i]);
         }
 
-        for(auto i : m){
-            if(i.second.size()>1){
+        for(int i = 0;i<1000;i++){
+            if(a[i].size()>1 || b[i].size()>1){
                 return false;
             }
         }
 
-        for(auto i : p){
-            if(i.second.size()>1){
-                return false;
-            }
-        }
-        return true;
+        return true; 
     }
 };
