d901e4b45fc6d67e405744edc0db887e238e957b
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2023-07-27 01:54:45 +0530

    Time: 316 ms (7.57%) | Memory: 80.4 MB (20.23%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+        int win = k+1;
+        map<int,int> m;
+        for(int i= 0;i<min(win,n);i++ ){
+            if(m[nums[i]]>0){
+                return true;
+            }
+            m[nums[i]]++;
+        }
+        if(k>=n) return false;
+
+        for(int i = win;i<n;i++){
+            m[nums[i-win]] = 0;
+            if(m[nums[i]]>0){
+                return true;
+            }
+            m[nums[i]]++;
+        }
+
+        return false;
+    }
+};
