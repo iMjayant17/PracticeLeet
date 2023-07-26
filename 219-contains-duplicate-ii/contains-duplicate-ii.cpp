ae87df7a49ed56530da22fb0f72ec40e769cf343
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2023-07-27 02:45:22 +0530

    Time: 323 ms (7.26%) | Memory: 80.4 MB (25.11%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -2,76 +2,79 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         
+        // int n = nums.size(); 
+        // int winsize = k+1;
+
+        // map<int,int> m;
+
+        // for(int i = 0;i<min(winsize,n);i++){
+        //     if(m[nums[i]]>0){
+        //         return true;
+        //     }
+        //     m[nums[i]]++;
+        // }
+
+        // for(int i = 0;i<(n- winsize);i++){
+        //     m[nums[i]] = 0;
+        //     if(m[nums[i+winsize]]>0){
+        //         return true;
+        //     }
+        //     m[nums[i+winsize]]++;
+        // }        
+
+        // return false;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         int n = nums.size();
-        int winsize = k+1;
-
+        int win = k+1;
         map<int,int> m;
-
-        for(int i = 0;i<min(winsize,n);i++){
+        for(int i= 0;i<min(win,n);i++ ){
             if(m[nums[i]]>0){
                 return true;
             }
             m[nums[i]]++;
         }
-
-        for(int i = 0;i<(n- winsize);i++){
-            m[nums[i]] = 0;
-            if(m[nums[i+winsize]]>0){
-                return true;
-            }
-            m[nums[i+winsize]]++;
-        }        
-
-        return false;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // int n = nums.size();
-        // int win = k+1;
-        // map<int,int> m;
-        // for(int i= 0;i<min(win,n);i++ ){
-        //     if(m[nums[i]]>0){
-        //         return true;
-        //     }
-        //     m[nums[i]]++;
-        // }
         // if(k>=n) return false;
 
-        // for(int i = win;i<n;i++){
-        //     m[nums[i-win]] = 0;
-        //     if(m[nums[i]]>0){
-        //         return true;
-        //     }
-        //     m[nums[i]]++;
-        // }
+        for(int i = win;i<n;i++){
 
-        // return false;
+            m[nums[i-win]] = 0; 
+
+            if(m[nums[i]]>0){
+                return true;
+            }
+
+            m[nums[i]]++;
+        }
+
+        return false;
     }
 };
