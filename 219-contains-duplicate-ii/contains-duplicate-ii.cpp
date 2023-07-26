6c0845ac2f16c339900651772f2e34f6816e5485
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2023-07-27 02:42:23 +0530

    Time: 314 ms (7.68%) | Memory: 80.4 MB (20.23%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,25 +1,77 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
         int n = nums.size();
-        int win = k+1;
-        map<int,int> m;
-        for(int i= 0;i<min(win,n);i++ ){
-            if(m[nums[i]]>0){
-                return true;
-            }
-            m[nums[i]]++;
-        }
-        if(k>=n) return false;
+        int winsize = k+1;
 
-        for(int i = win;i<n;i++){
-            m[nums[i-win]] = 0;
+        map<int,int> m;
+
+        for(int i = 0;i<min(winsize,n);i++){
             if(m[nums[i]]>0){
                 return true;
             }
             m[nums[i]]++;
         }
+
+        for(int i = 0;i<(n- winsize);i++){
+            m[nums[i]] = 0;
+            if(m[nums[i+winsize]]>0){
+                return true;
+            }
+            m[nums[i+winsize]]++;
+        }        
 
         return false;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // int n = nums.size();
+        // int win = k+1;
+        // map<int,int> m;
+        // for(int i= 0;i<min(win,n);i++ ){
+        //     if(m[nums[i]]>0){
+        //         return true;
+        //     }
+        //     m[nums[i]]++;
+        // }
+        // if(k>=n) return false;
+
+        // for(int i = win;i<n;i++){
+        //     m[nums[i-win]] = 0;
+        //     if(m[nums[i]]>0){
+        //         return true;
+        //     }
+        //     m[nums[i]]++;
+        // }
+
+        // return false;
     }
 };
