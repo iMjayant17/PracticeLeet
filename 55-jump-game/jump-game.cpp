8eb1e6f3a65a40367bf5f02e1e65794e33f8dd9b
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2023-07-29 04:05:35 +0530

    Time: 55 ms (98.28%) | Memory: 48.5 MB (40.38%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,77 +1,44 @@
-class Solution {
-public:
-    bool canJump(vector<int>& nums) {
+class Solution
+{
+    public:
+        bool canJump(vector<int> &nums)
+        {
 
-            stack<int> s;
+            vector<int> s;
             int n = nums.size();
-           
-            // if(nums[0]==0 && nums.size()>1){
-            //     return false;
-            // }
-            
-            for(int i = n-2;i>=0;i--){
-                if(nums[i]==0){
-                    s.push(i);
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (nums[i] == 0)
+                {
+                    s.push_back(i);
                 }
-                else{
-                    while(s.size()>0 && s.top()<i+nums[i]){
-                        s.pop();
+                else
+                {
+                    while (s.size() > 0 && s.back() < i + nums[i])
+                    {
+                        s.pop_back();
                     }
                 }
-
             }
 
-            if(s.size()==0) return true;
-            else{
-                return false;
-            }
+            if (s.size() == 0) return true;
+            return false;
 
+           	// vector<int> ind;
+           	// int n = nums.size();
+           	// if(nums[0] ==0 && n>1) return false;
+           	// for(int i =n-2;i>=0;i--){
+           	//     if(nums[i]==0){
+           	//         ind.push_back(i+1);
+           	//         continue;
+           	//     }
+           	//     while(ind.size()>0 && ind.back()<= i+ nums[i]){
+           	//         ind.pop_back();
+           	//     }
+           	// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // vector<int> ind;
-        // int n = nums.size();
-        // if(nums[0] ==0 && n>1) return false;
-        // for(int i =n-2;i>=0;i--){
-        //     if(nums[i]==0){
-        //         ind.push_back(i+1);
-        //         continue;
-        //     }
-        //     while(ind.size()>0 && ind.back()<= i+ nums[i] ){
-        //         ind.pop_back();
-        //     }
-        // }
-
-        // if(ind.size()==0) return true;
-        // return false;
-    }
+           	// if(ind.size()==0) return true;
+           	// return false;
+        }
 };
