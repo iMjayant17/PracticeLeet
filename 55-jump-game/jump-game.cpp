dcba4596f4a1c220e28eabbac4555efb25f80412
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2023-07-29 04:03:54 +0530

    Time: 61 ms (93.33%) | Memory: 48.7 MB (22.51%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,20 +1,79 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<int> ind;
-        int n = nums.size();
-        if(nums[0] ==0 && n>1) return false;
-        for(int i =n-2;i>=0;i--){
-            if(nums[i]==0){
-                ind.push_back(i+1);
-                continue;
-            }
-            while(ind.size()>0 && ind.back()<= i+ nums[i] ){
-                ind.pop_back();
-            }
-        }
 
-        if(ind.size()==0) return true;
-        return false;
+            stack<int> s;
+            int n = nums.size();
+            if(n==1){
+                return true;
+            }
+            if(nums[0]==0 && nums.size()>1){
+                return false;
+            }
+            
+            for(int i = n-2;i>=0;i--){
+                if(nums[i]==0){
+                    s.push(i);
+                }
+                else{
+                    while(s.size()>0 && s.top()<i+nums[i]){
+                        s.pop();
+                    }
+                }
+
+            }
+
+            if(s.size()==0) return true;
+            else{
+                return false;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // vector<int> ind;
+        // int n = nums.size();
+        // if(nums[0] ==0 && n>1) return false;
+        // for(int i =n-2;i>=0;i--){
+        //     if(nums[i]==0){
+        //         ind.push_back(i+1);
+        //         continue;
+        //     }
+        //     while(ind.size()>0 && ind.back()<= i+ nums[i] ){
+        //         ind.pop_back();
+        //     }
+        // }
+
+        // if(ind.size()==0) return true;
+        // return false;
     }
 };
