90ac3d001748d995df061cb9cb4434e964662cd6
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2023-09-04 20:16:17 +0530

    Time: 26 ms (88.21%) | Memory: 12.6 MB (56.39%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
 
 
-    void dfs(int i,int j ,vector<vector<char>>&grid,vector<vector<char>>&visited){
+    void solve(int i,int j ,vector<vector<char>>&grid,vector<vector<char>>&visited){
         if(i>=grid.size() || i<0 || j>=grid[0].size() || j<0){
             return;
         }
         if(grid[i][j]==visited[i][j] && visited[i][j]=='1'){
             visited[i][j]='0';
-        dfs(i+1,j,grid,visited);
-        dfs(i-1,j,grid,visited);
-        dfs(i,j+1,grid,visited);
-        dfs(i,j-1,grid,visited);
+        solve(i+1,j,grid,visited);
+        solve(i-1,j,grid,visited);
+        solve(i,j+1,grid,visited);
+        solve(i,j-1,grid,visited);
         }
-        return;
+    
         
     }
 
@@ -27,7 +27,7 @@ public:
             for(int j = 0;j<grid[0].size();j++){
                 if(grid[i][j]==visited[i][j] && visited[i][j]=='1'){
                     ans++;
-                    dfs(i,j,grid,visited);
+                    solve(i,j,grid,visited);
                 }
             }
         }
