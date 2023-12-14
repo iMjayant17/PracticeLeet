48c235912bf8749d891e379e74ddc328b785ad9c
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2023-12-15 01:10:55 +0530

    Time: 187 ms (9.90%) | Memory: 49.7 MB (13.36%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -7,20 +7,17 @@ public:
             dp[0][i] = INT_MAX;
         }
         dp[0][0] = 0;
+
         for(int i = 1;i<n+1;i++){
             for(int j = 1;j<amount+1;j++){
                 dp[i][j] = dp[i-1][j];
+
                 if(j-coins[i-1]>=0 && dp[i][j-coins[i-1]]!=INT_MAX){
                     dp[i][j] = min(dp[i][j],1+dp[i][j-coins[i-1]]);
                 }
+                
             }
         }
-        // for(auto i:dp){
-        //     for(auto j:i){
-        //         cout<<j<<" ";
-        //     }
-        //     cout<<endl;
-        // }
         if(dp[n][amount]==INT_MAX){
             return -1;
         }
