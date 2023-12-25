2c53053735fe94cd111b644cf04b9142c7bdb070
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2023-12-26 01:22:15 +0530

    Time: 7 ms (19.35%) | Memory: 9.9 MB (24.08%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        cout<<n<<" "<<m<<endl;
-        int e = (n*m) -1;
-        int s = 0;
-        while(s<=e){
-            int mid = (s+e)/2;
-            int ele = matrix[mid/m][mid%m];
-            if(ele==target){
-                return true;
-            } 
-            else if(ele < target){
-                s = mid+1;
-            }
-            else{
-                e = mid-1;
-            }
-        }
+    bool searchMatrix(vector<vector<int>>& A, int B) {
+        // int n = matrix.size();
+        // int m = matrix[0].size();
+        // cout<<n<<" "<<m<<endl;
+        // int e = (n*m) -1;
+        // int s = 0;
+        // while(s<=e){
+        //     int mid = (s+e)/2;
+        //     int ele = matrix[mid/m][mid%m];
+        //     if(ele==target){
+        //         return true;
+        //     } 
+        //     else if(ele < target){
+        //         s = mid+1;
+        //     }
+        //     else{
+        //         e = mid-1;
+        //     }
+        // }
 
-        return false;
+        // return false;
+
+        int n = A.size();
+    int m = A[0].size();
+    int i = 0;
+    int j = m-1;
+    while(i<n && j>=0){
+        if(A[i][j]==B) return 1;
+        if(A[i][j]<B) i++;
+        else j--;
+    }
+    return 0;
     }
 };
