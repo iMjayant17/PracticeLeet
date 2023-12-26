612a8940557193becd2c062db9e0f9da7b9a0833
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2023-12-26 20:31:54 +0530

    Time: 129 ms (5.29%) | Memory: 10.8 MB (22.31%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    const int mod = (int)pow(10, 9) + 7; 
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
-        return recursion(dp, n, k, target); 
-    }
-
-public:
-    int recursion(vector<vector<int>>& dp, int n, int k, int target) {
-        if (target == 0 && n == 0) return 1;
-        if (n == 0 || target <= 0) return 0;
-        if (dp[n][target] != -1) return dp[n][target] % mod;
-        int ways = 0;
-        for (int i = 1; i <= k; i++) {
-            ways = (ways + recursion(dp, n - 1, k, target - i)) % mod;
+        long long M = 1e9+7;
+        vector<vector<long long>> dp(n+1,vector<long long> (target+1,0));
+        dp[0][0] = 1;
+        for(long long i = 1;i<n+1;i++){
+            for(long long j =1;j<target+1;j++){
+                for(long long p = 1;p<=k;p++){
+                    if(j-p<0) continue;
+                    long long val = (dp[i][j]+ dp[i-1][j-p])%M;
+                    dp[i][j] = val;
+                    dp[i][j] = dp[i][j]%M;
+                }
+            }
         }
-        dp[n][target] = ways % mod; 
-        return dp[n][target];
+
+        return (int) dp[n][target]%M;
     }
 };
