8f83165019df6bafcb2fd9218dd946f06b211676
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-02-01 03:44:38 +0530

    Time: 4 ms (67.78%) | Memory: 11.5 MB (17.46%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,74 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        // vector<int> ans;
-        // for(int i = 0;i<nums1.size();i++){
-        //     int a = nums1[i];
-        //     int val = -1;
-        //     int j = 0;
-        //     for(;j<nums2.size();j++){
-        //         if(nums2[j]==a){
-        //             break;
-        //         }
-        //     }
-            
-        //     for(;j<nums2.size();j++){
-        //         if(nums2[j]>a){
-        //             val = nums2[j];
-        //             break;
-        //         }
-        //     }
-        //     ans.push_back(val);
-        // }
-
-        // return ans;
-
-
-        // NEXT GREATER ELEMENT LOGIC
-        vector<int > ans;
-        vector<int > temp(10004,-1);
-        int n = nums2.size()-1;
-        stack<int > s;
-        s.push(nums2[n]);
+        map<int,int> m;
+        int n = nums2.size();
+        vector<int> temp(n);
+        stack<int> s;
         for(int i = n-1;i>=0;i--){
-            while(s.size()>0 && s.top()<nums2[i]){
-                // cout<<s.top()<<endl;
+            while(s.size()>0 && s.top()<=nums2[i]){
                 s.pop();
             }
-
-            if(s.size()>0){
-                // cout<<s.top()<<endl;
-                temp[nums2[i]] = s.top();
+            if(s.size()==0){
+                temp[i] = -1;
+                m[nums2[i]] = temp[i];
+            }
+            else{
+                temp[i] = s.top();
+                m[nums2[i]] = temp[i];
             }
             s.push(nums2[i]);
         }
-
-        // for(int i = 0;i<10;i++){
-        //     cout<<temp[i]<<" ";
-        // }
-
-        // cout<<endl;
-            for(auto i: nums1){
-                ans.push_back(temp[i]);
-            }
+        vector<int> ans;
+        for(auto i:nums1){
+                ans.push_back(m[i]);
+        }
         return ans;
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 };
