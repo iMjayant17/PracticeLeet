4b8aeb8e18daf9b6701b3b2608a606e10960ceb9
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-02-05 03:14:16 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (37.27%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    void subs(vector<int>& nums,int i,vector<vector<int>> &ans,vector<int> storing){
+        // base case
+        if(i>nums.size()-1){
+            ans.push_back(storing);
+            return;
+        }
+        // exclude
+        subs(nums,i+1,ans,storing);
+        storing.push_back(nums[i]);
+        subs(nums,i+1,ans,storing);
+        // storing.pop_back();
+
+    }
+
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> storing;
+        subs(nums,0,ans,storing);
+        return ans;
+    }
+};
