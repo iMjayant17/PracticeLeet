8cb01e828418a584543a33236c5b8080d8da8bf0
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-02-06 13:24:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (35.80%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -3,24 +3,22 @@ public:
 vector<vector<int>> ans;
 vector<int> curr;
 void solve(vector<int> &arr,int sizee,int target,int idx){
+    
     if(sizee==0 && target==0){
-        set<int> s;
-        for(auto i : curr){
-            s.insert(i);
-        }
-        if(s.size()==curr.size() && is_sorted(curr.begin(),curr.end())) ans.push_back(curr);
-
+        ans.push_back(curr);
         return;
     }
     if(sizee<0 || target<0){
         return;
     }
-    for(int i = idx;i<arr.size();i++){
-        curr.push_back(arr[i]);
-        solve(arr,sizee-1,target-arr[i],idx+1);
-        curr.pop_back();
-        // solve(arr,sizee,target,idx+1);
+    if(idx>=arr.size()){
+        return;
     }
+    curr.push_back(arr[idx]);
+    solve(arr,sizee-1,target-arr[idx],idx+1);
+    curr.pop_back();
+    solve(arr,sizee,target,idx+1);
+    
     
 }
     vector<vector<int>> combinationSum3(int k, int n) {
