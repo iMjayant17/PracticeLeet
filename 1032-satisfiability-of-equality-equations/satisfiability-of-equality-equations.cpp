edd340d90efe21a74f67cc2483987b620b6f5e9b
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-05-11 13:12:37 +0530

    Time: 5 ms (46.89%) | Memory: 15 MB (42.09%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.cpp
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.cpp
@@ -1,28 +1,56 @@
 class Solution {
 public:
-    int find(int i,vector<int> & parent){
-        if(parent[i] == i) return i;
-        return find(parent[i],parent);
-    } 
+/*
+Normal DSU
+*/
+    // int find(int i,vector<int> & parent){
+    //     if(parent[i] == i) return i;
+    //     return find(parent[i],parent);
+    // } 
 
-    void Union(int x,int y,vector<int> &parent){
+    // void Union(int x,int y,vector<int> &parent){
+    //     int x_par = find(x,parent);
+    //     int y_par = find(y,parent);
+
+    //     if(x_par !=y_par){
+    //         parent[x_par] = y_par;
+    //     }
+    // }
+
+    /*
+    DSU by path compression and rank
+    */
+
+    int find(int i,vector<int> & parent){
+        if(i==parent[i]) return i;
+        return parent[i] = find(parent[i],parent);
+    }
+
+    void Union(int x,int y,vector<int> &parent ,vector<int> &rank ){
         int x_par = find(x,parent);
         int y_par = find(y,parent);
 
-        if(x_par !=y_par){
+        if(rank[x_par]<rank[y_par]){
             parent[x_par] = y_par;
+        }
+        else if(rank[y_par]<rank[x_par]){
+            parent[y_par] = x_par;
+        }
+        else{
+            parent[y_par] = x_par;
+            rank[x_par]++;
         }
     }
 
     bool equationsPossible(vector<string>& equations) {
-        vector<int> parent(26);
+        vector<int> parent(26),rank(26,0);
         for(int i = 0;i<26;i++){
             parent[i] = i;
         }
 
         for(auto s:equations){
             if(s[1]=='='){
-                Union(s[0]-'a',s[3]-'a',parent);
+                Union(s[0]-'a',s[3]-'a',parent,rank);
             }
         }
 
