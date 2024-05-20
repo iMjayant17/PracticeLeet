5f39c2d76b997294d5ffd876136880e9eeb83644
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-05-20 15:27:05 +0530

    Time: 34 ms (68.37%) | Memory: 82.9 MB (5.02%) - LeetSync

--- 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -1,3 +1,5 @@
+/*
+                           with DFS
 class Solution {
 public:
 
@@ -26,5 +28,57 @@ public:
         }
 
         return n - ans;
+    }
+};
+
+*/
+
+
+// with DSU
+class Solution {
+public:
+
+    vector<int> parent,rank;
+
+    int find(int i){
+        if(parent[i]==i) return i;
+        return parent[i] = find(parent[i]);
+    }
+
+    void Union(int x,int y){
+        int x_par = find(x);
+        int y_par = find(y);
+
+        if(rank[x_par]<rank[y_par]){
+            parent[x_par] = y_par;
+        }
+        else{
+            parent[x_par] = y_par;
+            rank[y_par]++;
+        }
+    }
+    
+    int removeStones(vector<vector<int>>& stones) {
+        
+        int n = 100005;
+        rank.resize(n); 
+        parent.resize(n);
+        for(int i = 0;i<n;i++){
+            parent[i] = i;
+        } 
+
+
+        for(auto st:stones){
+            Union(st[0], st[1]+10008);
+        }
+        set<int> st;
+        for(auto i:stones){
+            int a = find(i[0]);
+            int b = find(i[1]+10008);
+            st.insert(a);
+            st.insert(b);
+        }
+
+        return stones.size() - st.size();
     }
 };
