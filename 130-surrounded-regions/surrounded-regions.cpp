ef9f6310053eb55b730241bae4365d43cb717f03
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-05-23 14:27:36 +0530

    Time: 14 ms (31.66%) | Memory: 13.4 MB (96.61%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
-    bool chk(vector<vector<char>>& board,int i ,int j){
-        if(i>=board.size() || i<0 || j>=board[0].size() || j<0){
-            return false;
-        }
-        if(board[i][j]=='X') return true;
-        board[i][j]='X';
-        bool a = chk(board,i+1,j);
-        board[i][j]='O';
-        if(!a) return false;
-        board[i][j]='X';
-        bool b = chk(board,i-1,j);
-        board[i][j]='O';
-        if(!b) return false;
-        board[i][j] = 'X';
-        bool c = chk(board,i,j+1);
-        board[i][j]='O';
-        if(!c) return false;
-        board[i][j] = 'X';
-        bool d = chk(board,i,j-1);
-        board[i][j]='O';
-        if(!d) return false;
-        return true;
-    }
+    // bool chk(vector<vector<char>>& board,int i ,int j){
+    //     if(i>=board.size() || i<0 || j>=board[0].size() || j<0){
+    //         return false;
+    //     }
+    //     if(board[i][j]=='X') return true;
+    //     board[i][j]='X';
+    //     bool a = chk(board,i+1,j);
+    //     board[i][j]='O';
+    //     if(!a) return false;
+    //     board[i][j]='X';
+    //     bool b = chk(board,i-1,j);
+    //     board[i][j]='O';
+    //     if(!b) return false;
+    //     board[i][j] = 'X';
+    //     bool c = chk(board,i,j+1);
+    //     board[i][j]='O';
+    //     if(!c) return false;
+    //     board[i][j] = 'X';
+    //     bool d = chk(board,i,j-1);
+    //     board[i][j]='O';
+    //     if(!d) return false;
+    //     return true;
+    // }
 
 
-    void fill(vector<vector<char>>& board,int i ,int j){
-        if(i>=board.size() || i<0 || j>=board[0].size() || j<0){
-            return;
-        }
-        if(board[i][j]=='X') return;
-        board[i][j] = 'X';
-        fill(board,i+1,j);
-        fill(board,i-1,j);
-        fill(board,i,j+1);
-        fill(board,i,j-1);
+    // void fill(vector<vector<char>>& board,int i ,int j){
+    //     if(i>=board.size() || i<0 || j>=board[0].size() || j<0){
+    //         return;
+    //     }
+    //     if(board[i][j]=='X') return;
+    //     board[i][j] = 'X';
+    //     fill(board,i+1,j);
+    //     fill(board,i-1,j);
+    //     fill(board,i,j+1);
+    //     fill(board,i,j-1);
         
-    }
+    // }
     void fill2(vector<vector<char>>& board,int i ,int j){
         if(i>=board.size() || i<0 || j>=board[0].size() || j<0){
             return;
@@ -50,20 +50,7 @@ public:
         
     }
     void solve(vector<vector<char>>& board) {
-        // for(int i = 0;i<board.size();i++){
-        //     for(int j = 0;j<board[0].size();j++){
-        //         if(board[i][j]=='O'){
-        //             if(chk(board,i,j)){
-        //                 fill(board,i,j);
-        //                 // cout<<i<<" "<<j<<endl;
-        //             }
-        //             else{
-        //                 // cout<<i<<" "<<j<<endl;
-        //                 fill2(board,i,j);
-        //             }
-        //         }
-        //     }
-        // }
+
         int n = board.size();
         int m = board[0].size();
         for(int i = 0;i<n;i++){
