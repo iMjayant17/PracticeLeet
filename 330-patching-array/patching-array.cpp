aec3681ba1efa922df2e4dfeb4d88234711fdec9
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-06-19 03:04:16 +0530

    Time: 3 ms (84.06%) | Memory: 13.5 MB (98.30%) - LeetSync

---
+++ 330-patching-array/patching-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long sum = 0;
+        int cnt = 0;
+        int i = 0;
+        while(sum<n && i<nums.size()){
+            while(i<nums.size() && sum+1<nums[i] && sum<n){
+                sum+= (sum+1);
+                cnt++;
+            }
+            if(i<nums.size() && sum+1>=nums[i]){
+                sum+= nums[i];
+                i++;
+            }
+
+            if(i>=nums.size()){
+                 break;
+            }
+            
+        }
+        if(i>=nums.size() && sum<n){
+            while( sum<n){
+                sum += (sum+1);
+                cnt++;
+            }
+        }
+        return cnt;
+        
+    }
+};
