1cf1edeea21370cb8b1edca46f469ae85eb971d9
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-06-21 21:36:22 +0530

    Time: 22 ms (72.60%) | Memory: 35 MB (7.68%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -10,22 +10,15 @@ public:
         }
         int maxi = 0;
         int sum = 0;
-        // for(auto i:temp){
-        //     cout<<i<<" ";
-        // }
-        // cout<<endl;
         for(int i = 0;i<min(m,n);i++){
             sum+=temp[i];
-            // cout<<temp[i]<<" ";
         }
-        // cout<<endl;
+
         maxi = max(sum,maxi);
         int s = 0,e = m;
-        // cout<<ans<<" "<<maxi<<endl;
         while(e<n){
             sum+= temp[e++];
             sum-= temp[s++];
-            // cout<<sum<<endl;
             maxi = max(sum,maxi);
         } 
 
