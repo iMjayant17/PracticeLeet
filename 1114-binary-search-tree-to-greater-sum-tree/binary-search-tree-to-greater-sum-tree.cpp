b17960042ef424265eb032aa34362157f9c5c8db
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-06-25 20:37:00 +0530

    Time: 5 ms (15.49%) | Memory: 10.2 MB (12.64%) - LeetSync

--- 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.cpp
@@ -6,30 +6,44 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
+    // void dfs(TreeNode* root,TreeNode* Root,int &sum){
+    //     if(root==NULL) return;
 
-    void dfs(TreeNode* root,TreeNode* Root,int &sum){
-        if(root==NULL) return;
+    //     if(root->right != NULL ) Root->right = new TreeNode();
+    //     dfs(root->right,Root->right,sum);
+    //     if(root!=NULL){
+    //         sum+=root->val;
+    //         Root->val = sum;
+    //     }
+    //     if(root->left != NULL ) Root->left = new TreeNode();
+    //     dfs(root->left,Root->left,sum);
+    // }
 
-        if(root->right != NULL ) Root->right = new TreeNode();
-        dfs(root->right,Root->right,sum);
-        if(root!=NULL){
-            sum+=root->val;
-            Root->val = sum;
-            // cout<<sum<<endl;
-        }
-        if(root->left != NULL ) Root->left = new TreeNode();
-        dfs(root->left,Root->left,sum);
+    // TreeNode* bstToGst(TreeNode* root) {
+    //     int sum = 0;
+    //     TreeNode* Root = new TreeNode();
+    //     dfs(root,Root,sum);
+    //     return Root;
+    // }
+
+    void dfs(TreeNode* root, int& sum) {
+        if (root == NULL)
+            return;
+        dfs(root->right, sum);
+        sum += root->val;
+        root->val = sum;
+        dfs(root->left, sum);
     }
 
     TreeNode* bstToGst(TreeNode* root) {
         int sum = 0;
-        TreeNode* Root = new TreeNode(); 
-        dfs(root,Root,sum);
-        return Root;
+        dfs(root, sum);
+        return root;
     }
 };
