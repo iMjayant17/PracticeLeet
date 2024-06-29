af5debdd1a2ec06ce7a1a265efc4888e45b98ebb
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-06-30 05:19:26 +0530

    Time: 1464 ms (13.59%) | Memory: 89.8 MB (79.44%) - LeetSync

--- 3305-count-prefix-and-suffix-pairs-ii/count-prefix-and-suffix-pairs-ii.cpp
+++ 3305-count-prefix-and-suffix-pairs-ii/count-prefix-and-suffix-pairs-ii.cpp
@@ -12,7 +12,7 @@ public:
             for(int j=0 ; j<words[i].size() ; j++){
                 w1+=words[i][j];
                 w2+=words[i][words[i].size()-1-j];
-                if(mp.count(w1)){
+                if(mp.find(w1)!=mp.end()){
                     string r=w2;
                     reverse(r.begin(),r.end());
                     if(r==w1){
