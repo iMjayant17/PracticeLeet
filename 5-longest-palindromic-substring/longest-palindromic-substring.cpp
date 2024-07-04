ff35679b47f85cc6e05a8e4ed1869877ba3c1840
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-07-04 09:56:24 +0530

    Time: 101 ms (45.79%) | Memory: 12.5 MB (63.89%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,13 +1,13 @@
 class Solution {
-
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
+        int dp[1005][1005];
+        memset(dp,0,sizeof(dp));
+        int n = s.length();
         int maxi = 0;
-        string ans = "";
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        for(int diff=0; diff<n; diff++){
-            for(int i=0, j=diff; j<n; i++, j++){
+        int ind = 0;
+        for(int diff = 0;diff<n;diff++){
+            for(int i = 0,j = diff;j<n;i++,j++){
                 if(diff==0){
                     dp[i][j] = 1;
                 }
@@ -15,24 +15,18 @@ public:
                     if(s[i]==s[j]) dp[i][j] = 2;
                 }
                 else{
-                    if(s[i]==s[j] && dp[i+1][j-1]>0){
-                        dp[i][j] = 2 + dp[i+1][j-1]; 
+                    if(dp[i+1][j-1]>0 && s[i]==s[j]){
+                        dp[i][j] = dp[i+1][j-1] + 2;
                     }
                 }
 
-                if( dp[i][j]>0 && j-i+1 > maxi){
-                    maxi = j-i+1;
-                    ans = s.substr(i,maxi);
+                if(maxi<dp[i][j]){
+                   maxi = dp[i][j];
+                    ind = i;
                 }
             }
         }
-        // for(auto i:dp){
-        //     for(auto j:i){
-        //         cout<<j<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        return ans;
-        
+
+        return s.substr(ind,maxi);
     }
 };
