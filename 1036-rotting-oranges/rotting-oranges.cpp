251225b2c0086dd048361f94d94598b8bc283d9e
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-07-04 11:14:18 +0530

    Time: 5 ms (57.88%) | Memory: 15.8 MB (83.86%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -2,56 +2,50 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         queue<pair<int,int>> Q;
-        int cnt = 0;
-        for(int i = 0;i<grid.size();i++){
-            for(int j = 0;j<grid[0].size();j++){
-                if(grid[i][j]==2) Q.push({i,j});
-                if(grid[i][j]==1) cnt++;
+        int n = grid.size();
+        int m = grid[0].size();
+        int total = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j]==2){
+                    Q.push({i,j});
+                }
+                if(grid[i][j]!=0) total++;
             }
         }
         int ans = 0;
-        if(Q.size()==0 && cnt == 0) return 0;
         while(Q.size()>0){
-
-            int len = Q.size();
-            for(int k = 0;k<len;k++){
+            int sz = Q.size();
+            for(int k = 0;k<sz;k++){
                 auto [i,j] = Q.front();
                 Q.pop();
-                if(i+1<grid.size()){
-                    if(grid[i+1][j]==1){
-                        grid[i+1][j] = 2;
-                        Q.push({i+1,j});
-                    }
+                total--;
+                if(i+1<n && grid[i+1][j]==1){
+                    Q.push({i+1,j});
+                    grid[i+1][j] = 2; 
+                    
+                } 
+
+                if(j+1<m && grid[i][j+1]==1){
+                    Q.push({i,j+1});
+                    grid[i][j+1] = 2; 
                 }
-                if(i-1>=0){
-                    if(grid[i-1][j]==1){
-                        grid[i-1][j] = 2;
-                        Q.push({i-1,j});
-                    }
-                }
-                if(j+1<grid[0].size()){
-                    if(grid[i][j+1]==1){
-                        grid[i][j+1] = 2;
-                        Q.push({i,j+1});
-                    }
-                }
-                if(j-1>=0){
-                    if(grid[i][j-1]==1){
-                        grid[i][j-1] = 2;
-                        Q.push({i,j-1});
-                    }
+
+                if(i-1>=0 && grid[i-1][j]==1){
+                    Q.push({i-1,j});
+                    grid[i-1][j] = 2; 
+                } 
+
+                if(j-1>=0 && grid[i][j-1]==1){
+                    Q.push({i,j-1});
+                    grid[i][j-1] = 2; 
                 }
             }
-
             ans++;
         }
 
-        for(int i = 0;i<grid.size();i++){
-            for(int j =0;j<grid[0].size();j++ ){
-                if(grid[i][j]==1) return -1;
-            }
-        }
-        
-        return ans-1;
+        if(total) return -1;
+        return max(0,ans-1);
+
     }
 };
