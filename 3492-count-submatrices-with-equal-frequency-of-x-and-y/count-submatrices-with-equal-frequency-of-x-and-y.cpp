d8d321ad8c6d3460be6e6568dd704aeb37e8deec
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-07-09 16:43:34 +0530

    Time: 440 ms (21.93%) | Memory: 128.2 MB (87.69%) - LeetSync

--- 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ 3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -4,27 +4,27 @@ public:
         int n = grid.size(), m = grid[0].size();
         int ans = 0;
 
-        vector<vector<pair<int, int>>> dp(n, vector<pair<int, int>>(m, {0, 0}));
+        vector<vector<pair<int, int>>> arr(n, vector<pair<int, int>>(m, {0, 0}));
 
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
 
                 if (i - 1 >= 0)
-                    dp[i][j].first += dp[i - 1][j].first,
-                        dp[i][j].second += dp[i - 1][j].second;
+                    arr[i][j].first += arr[i - 1][j].first,
+                        arr[i][j].second += arr[i - 1][j].second;
                 if (j - 1 >= 0)
-                    dp[i][j].first += dp[i][j - 1].first,
-                        dp[i][j].second += dp[i][j - 1].second;
+                    arr[i][j].first += arr[i][j - 1].first,
+                        arr[i][j].second += arr[i][j - 1].second;
                 if (i - 1 >= 0 && j - 1 >= 0)
-                    dp[i][j].first -= dp[i - 1][j - 1].first,
-                        dp[i][j].second -= dp[i - 1][j - 1].second;
+                    arr[i][j].first -= arr[i - 1][j - 1].first,
+                        arr[i][j].second -= arr[i - 1][j - 1].second;
                 if (grid[i][j] == 'X') {
-                    dp[i][j].first++;
+                    arr[i][j].first++;
                 } else if (grid[i][j] == 'Y') {
-                    dp[i][j].second++;
+                    arr[i][j].second++;
                 }
 
-                if (dp[i][j].first == dp[i][j].second && dp[i][j].first > 0)
+                if (arr[i][j].first == arr[i][j].second && arr[i][j].first > 0)
                     ans++;
             }
         }
