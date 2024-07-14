f98ce8172879f082d932ed6dee0cc88cf63edd29
_.jaYanT._ <115420374+iMjayant17@users.noreply.github.com>
2024-07-14 21:12:31 +0530

    Time: 7 ms (92.21%) | Memory: 13.9 MB (58.73%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,52 +1,15 @@
 class Solution {
 public:
-    // bool chk(vector<vector<char>>& board,int i ,int j){
-    //     if(i>=board.size() || i<0 || j>=board[0].size() || j<0){
-    //         return false;
-    //     }
-    //     if(board[i][j]=='X') return true;
-    //     board[i][j]='X';
-    //     bool a = chk(board,i+1,j);
-    //     board[i][j]='O';
-    //     if(!a) return false;
-    //     board[i][j]='X';
-    //     bool b = chk(board,i-1,j);
-    //     board[i][j]='O';
-    //     if(!b) return false;
-    //     board[i][j] = 'X';
-    //     bool c = chk(board,i,j+1);
-    //     board[i][j]='O';
-    //     if(!c) return false;
-    //     board[i][j] = 'X';
-    //     bool d = chk(board,i,j-1);
-    //     board[i][j]='O';
-    //     if(!d) return false;
-    //     return true;
-    // }
-
-
-    // void fill(vector<vector<char>>& board,int i ,int j){
-    //     if(i>=board.size() || i<0 || j>=board[0].size() || j<0){
-    //         return;
-    //     }
-    //     if(board[i][j]=='X') return;
-    //     board[i][j] = 'X';
-    //     fill(board,i+1,j);
-    //     fill(board,i-1,j);
-    //     fill(board,i,j+1);
-    //     fill(board,i,j-1);
-        
-    // }
-    void fill2(vector<vector<char>>& board,int i ,int j){
+    void fill(vector<vector<char>>& board,int i ,int j){
         if(i>=board.size() || i<0 || j>=board[0].size() || j<0){
             return;
         }
         if(board[i][j]!='O') return;
         board[i][j] = '1';
-        fill2(board,i+1,j);
-        fill2(board,i-1,j);
-        fill2(board,i,j+1);
-        fill2(board,i,j-1);
+        fill(board,i+1,j);
+        fill(board,i-1,j);
+        fill(board,i,j+1);
+        fill(board,i,j-1);
         
     }
     void solve(vector<vector<char>>& board) {
@@ -55,22 +18,22 @@ public:
         int m = board[0].size();
         for(int i = 0;i<n;i++){
             if(board[i][0]=='O'){
-                fill2(board,i,0);
+                fill(board,i,0);
             }
         }
         for(int i = 0;i<n;i++){
             if(board[i][m-1]=='O'){
-                fill2(board,i,m-1);
+                fill(board,i,m-1);
             }
         }
         for(int i = 0;i<m;i++){
             if(board[0][i]=='O'){
-                fill2(board,0,i);
+                fill(board,0,i);
             }
         }
         for(int i = 0;i<m;i++){
             if(board[n-1][i]=='O'){
-                fill2(board,n-1,i);
+                fill(board,n-1,i);
             }
         }
         for(int i = 0;i<board.size();i++){
