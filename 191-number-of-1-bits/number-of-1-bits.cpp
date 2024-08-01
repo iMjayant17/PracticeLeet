765737414bbef56d1040d88421febdd7402a65e2
._.jaYanT._. <115420374+iMjayant17@users.noreply.github.com>
2024-08-01 12:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (26.63%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        int count = 0;
-        while(n>0){
-            int lastbit = n%2;
-            if(lastbit==1){
-                count++;
-            }
-            n = n/2;
-        }
-
-        return count;
+    int hammingWeight(int n) {
+        return __builtin_popcount(n);
     }
 };
