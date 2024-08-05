11437ce9e41154b1df74be1b8cd96757e2fda69e
._.jaYanT._. <115420374+iMjayant17@users.noreply.github.com>
2024-08-05 19:41:35 +0530

    Time: 89 ms (72.94%) | Memory: 34.1 MB (78.45%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -1,57 +1,41 @@
 class Solution {
-public:
-
-void print(vector<vector<char>>& grid){
-    for(auto i: grid){
-        for(auto j:i){
-            cout<<j<<" ";
+ bool isvalid(int n,int m,int row,int col){
+        if(row>=0 and row<n and col>=0 and col<m){
+            return true;
         }
-        cout<<endl;
+        return false;
     }
-    cout<<endl;
-}
-    int nearestExit(vector<vector<char>>& grid, vector<int>& e) {
-
-        vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-        int ans = 0;
-
-        queue<pair<int, int>> q;
-        if (grid[e[0]][e[1]] == '+')
-            return -1;
-
-        q.push({e[0], e[1]});
-        grid[e[0]][e[1]] = '+';
-
-        while (q.size()) {
-            
-            int cnt = q.size();
-            for (int l = 0; l < cnt; l++) {
-
-                auto [i, j] = q.front();
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& ent) {
+        int n=maze.size();
+        int m=maze[0].size();
+        queue<pair<int,int>>q;
+        q.push(make_pair(ent[0],ent[1])); 
+        maze[ent[0]][ent[1]]='+';
+        int steps = 0;
+        vector<vector<int>>d= {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                pair<int,int>p = q.front();
                 q.pop();
-                if ((i == grid.size() - 1) || (j == grid[0].size() - 1) ||
-                    (i == 0) || (j == 0)) {
-                    if (i != e[0] || j != e[1])
-                        return ans;
+                int x = p.first;
+                int y = p.second;
+                if((x==0 or y==0 or x==n-1 or y==m-1)){
+                    if((x!=ent[0] or y!=ent[1]))
+                        return steps;
                 }
-                for (int p = 0; p < 4; p++) {
-
-                    int ii = i + dir[p][0];
-                    int jj = j + dir[p][1];
-                    if (ii >= grid.size() || ii < 0 || jj >= grid[0].size() ||
-                        jj < 0)
-                        continue;
-                    if (grid[ii][jj] == '+')
-                        continue;
-                    q.push({ii, jj});
-                    grid[ii][jj] = '+';
+                for(int k=0; k<4; k++){
+                    int nx= x+d[k][0];
+                    int ny= y+d[k][1];
+                    if(isvalid(n,m,nx,ny)==true and maze[nx][ny]!='+'){
+                    maze[nx][ny]='+';
+                    q.push(make_pair(nx,ny));
+                    }
                 }
-
-                // print(grid);
             }
-            ans++;
+            steps++;
         }
-
         return -1;
     }
 };
