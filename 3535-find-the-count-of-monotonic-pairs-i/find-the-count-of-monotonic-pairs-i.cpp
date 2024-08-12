3ef5abce18b7c854876507958394925eb8c88e33
._.jaYanT._. <115420374+iMjayant17@users.noreply.github.com>
2024-08-12 06:27:34 +0530

    Time: 898 ms (25.00%) | Memory: 58.2 MB (41.67%) - LeetSync

--- 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.cpp
+++ 3535-find-the-count-of-monotonic-pairs-i/find-the-count-of-monotonic-pairs-i.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    const int M = 1e9 + 7;
-    int dp[2005][60];
-    int solve(vector<int>& nums, int idx, int last) {
-        if (idx >= nums.size()) {
-            return 1;
-        }
-        if(dp[idx][last]!= -1) return dp[idx][last];
-        int ans = 0;
-        for (int i = 0; i <= nums[idx]; i++) {
-            int p1 = i, p2 = nums[idx] - i;
-            int lastval1 = last;
-            int lastval2 = 51-last;
-            if (idx - 1 >= 0) {
-                lastval2 = nums[idx-1] - last; 
-            }
-            if(p1>=lastval1 && p2<=lastval2){
-                ans = (ans+ (solve(nums,idx+1,i))%M)%M;
+    const int M = 1e9+7;
+    int dp[2005][55][55];
+    int solve(vector<int>& nums,int idx,int last_inc, int last_dec){
+        if(idx>=nums.size()) return 1;
+        if(dp[idx][last_inc][last_dec] != -1) return dp[idx][last_inc][last_dec];
+        int  ans = 0;
+        for(int i = 0;i<=nums[idx];i++){
+            int val1 = i;
+            int val2 = nums[idx]-val1;
+            if(val1>=last_inc && val2<=last_dec){
+                ans = (ans + solve(nums,idx+1,val1,val2)%M)%M; 
             }
         }
-
-        return dp[idx][last] =  ans%M;
+        return dp[idx][last_inc][last_dec] = ans%M;
     }
+
     int countOfPairs(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
-        return solve(nums,0,0);
+        return solve(nums,0,0,51);
     }
 };
