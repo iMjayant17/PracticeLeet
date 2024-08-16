e0f83798ef4443f534890d20c9b2e12fb85f4752
._.jaYanT._. <115420374+iMjayant17@users.noreply.github.com>
2024-08-17 03:45:42 +0530

    Time: 13 ms (51.16%) | Memory: 11.3 MB (67.89%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        
-        // x+y = sum
-        // x-y = target
-        // 2x = sum+target
-        // dp[i][j] = ith coin use krk jth sum bnane k ways
-
-        int sum = accumulate(nums.begin(),nums.end(),0ll);
-        if((sum+target)&1) return 0;
-        int tar = (sum+target)/2;
-        int n = nums.size();
-        vector<map<int,int>> dp(n+1); // special part...dimaag khraab
-        dp[0][0] = 1;
-        for(int i = 1;i<n+1;i++){
-            for(int j = 0;j<tar+1;j++){
-                dp[i][j] += dp[i-1][j];
-                if(j-nums[i-1]>=0) dp[i][j] += dp[i-1][j-nums[i-1]];
-            }
+    int dp[25][5000];
+    int solve(vector<int>& nums, int idx , int target){
+        if(idx==nums.size()){
+            return target==0;
         }
-        return dp[n][tar];
+        if(dp[idx][target+2000] != -1) return dp[idx][target+2000] ;
+        int a = solve(nums,idx+1,target-nums[idx]);
+        int b = solve(nums,idx+1,target+nums[idx]);
+        return dp[idx][target+2000] = a+b;
+    }
 
-
+    int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp,-1,sizeof(dp));
+        return solve(nums,0,target);
     }
 };
