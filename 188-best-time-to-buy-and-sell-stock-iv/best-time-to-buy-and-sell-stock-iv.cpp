8e91e125d72183e4f3f5b21d6e5b954e612c452d
._.jaYanT._. <115420374+iMjayant17@users.noreply.github.com>
2024-08-23 12:03:14 +0530

    Time: 55 ms (8.50%) | Memory: 23.1 MB (9.86%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    map<vector<int>,int> m;
+    int dp[1005][2005];
     int solve(vector<int>& arr,int idx,int t){
         if(t==0 || idx>=arr.size()) return 0;
-        if(m.find({idx,t})!= m.end()) return m[{idx,t}];
+        if(dp[idx][t]!= -1) return dp[idx][t];
         int a = 0,b = 0,c = 0,d = 0;
         if(t&1){
             b = arr[idx] + solve(arr,idx+1,t-1);
@@ -13,10 +13,11 @@ public:
             a = solve(arr,idx+1,t-1)-arr[idx];
             d = solve(arr,idx+1,t);
         }
-        return m[{idx,t}]=max({a,b,c,d});
+        return dp[idx][t]=max({a,b,c,d});
     }
 
     int maxProfit(int k, vector<int>& prices) {
+        memset(dp,-1,sizeof(dp));
         return solve(prices,0,2*k);
     }
 };
