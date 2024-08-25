fa1c7960b07b889187a5bebcf15b4d8b218e33a1
._.jaYanT._. <115420374+iMjayant17@users.noreply.github.com>
2024-08-25 06:56:47 +0530

    Time: 83 ms (25.25%) | Memory: 12.6 MB (63.46%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.cpp
+++ 923-super-egg-drop/super-egg-drop.cpp
@@ -5,21 +5,31 @@ public:
         if(n<=1) return n;
         if(k==1) return n;
         if(dp[k][n]!= -1) return dp[k][n];
-        int mini = INT_MAX;
+        int mini = 1000000;
         int s = 1,e = n;
-        while(s<=e){
+        while(s+1<e){
             int mid = e - (e-s)/2;
             int broken = solve(k-1,mid-1);
             int notBroken = solve(k,n-mid);
             int val = max(broken,notBroken);
+            mini = min(mini,val);
             if(broken<notBroken){
-                s = mid+1;
+                s = mid;
+            }
+            else if(broken>notBroken){
+                e = mid;
             }
             else{
-                e = mid-1;
+                break;
             }
-            mini = min(mini,val);
         }
+        int broken1 = solve(k-1,s-1);
+        int notBroken1 = solve(k,n-s);
+        int val1 = max(broken1,notBroken1);
+        int broken2 = solve(k-1,e-1);
+        int notBroken2 = solve(k,n-e);
+        int val2 = max(broken2,notBroken2);
+        mini = min(mini,min(val1,val2));
         return dp[k][n] = 1+mini;
     }
     int superEggDrop(int k, int n) {
