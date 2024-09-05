7dfb7fb6e8eb1b547083e89d01b35f2afae7138b
._.jaYanT._. <115420374+iMjayant17@users.noreply.github.com>
2024-09-05 12:22:21 +0530

    Time: 699 ms (28.36%) | Memory: 299.8 MB (7.25%) - LeetSync

--- 823-split-array-with-same-average/split-array-with-same-average.cpp
+++ 823-split-array-with-same-average/split-array-with-same-average.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    unordered_map<string, int> mp;
-    
-    bool helper(vector<int>& nums, int i, int n, int sum, int n1)
-    {
-        // base case
-        
-        if(n1 == 0)
+    map<string, int> mp;
+
+    bool helper(vector<int>& nums, int i, int n, int sum, int n1) {
+        if (n1 == 0)
             return sum == 0;
-        
-        if(i == n)
+        if (i == n)
             return false;
-        
-        // create a key
-        
         string key = to_string(i) + "#" + to_string(n1) + "#" + to_string(sum);
-        
-        // if already calculated
-        
-        if(mp.count(key))
+        if (mp.count(key))
             return mp[key];
-        
-        // if nums[i] <= sum, then we will either include or exclude
-        
-        // calculate the result and store in map
-       
-        if(nums[i] <= sum)
-        {
-            return mp[key] = helper(nums, i + 1, n, sum - nums[i], n1 - 1) || helper(nums, i + 1, n, sum, n1);
-        }
-        else
+
+        if (nums[i] <= sum) {
+            return mp[key] = helper(nums, i + 1, n, sum - nums[i], n1 - 1) ||
+                             helper(nums, i + 1, n, sum, n1);
+        } else
             return mp[key] = helper(nums, i + 1, n, sum, n1);
     }
 
@@ -47,13 +32,9 @@ public:
         vector<vector<vector<bool>>> dp(
             n, vector<vector<bool>>(n + 1, vector<bool>(sum + 1, false)));
         for (int i = 1; i < n; i++) {
-            if((sum * i) % n == 0)
-            {
+            if ((sum * i) % n == 0) {
                 int req_sum = (sum * i) / n;
-                
-                // check if there is any subset of n1 that has sum of req_sum
-            
-                if(helper(nums, 0, n, req_sum, i))
+                if (helper(nums, 0, n, req_sum, i))
                     return true;
             }
         }
