fa112d23bf9ac4df0e39f05c325231b1bbd62de1
._.jaYanT._. <115420374+iMjayant17@users.noreply.github.com>
2024-10-31 01:32:30 +0530

    Time: 3 ms (70.45%) | Memory: 11.8 MB (45.26%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -18,87 +18,34 @@ public:
     }
 };
 */
-    unordered_map<Node* ,Node*> m;
-
-void DFS(Node* node,Node* clone){
-    
-    for(Node* i:node->neighbors){
-        if(m.find(i)==m.end()){
-            Node* clone_node = new Node(i->val);
-            m[i] = clone_node;
-            clone->neighbors.push_back(clone_node);
-            DFS(i,clone_node);
-        }
-        else{
-            clone->neighbors.push_back(m[i]);
-        }
-    }
-}
 
 class Solution {
 public:
+
+    unordered_map<Node* ,Node*> m;
+
+    void dfs(Node* node,Node* clone){
+
+        for(Node* i:node->neighbors){
+            if(m.find(i)==m.end()){
+                Node* temp = new Node(i->val);
+                clone->neighbors.push_back(temp);
+                m[i] = temp;
+                dfs(i,temp);
+            }
+            else{
+                clone->neighbors.push_back(m[i]);
+            }
+        }
+    }
+
+
     Node* cloneGraph(Node* node) {
-        if(!node) return NULL;
+        if(!node) return node;
         m.clear();
         Node* clone_node = new Node(node->val);
         m[node] = clone_node;
-        DFS(node,clone_node);
+        dfs(node,clone_node);
         return clone_node;
     }
 };
-
-
-/*
-
-//Approach-2 DFS (Using unordered_map)
-class Solution {
-public:
-    
-    unordered_map<Node*, Node*> mp;
-    
-    void DFS(Node* node, Node* clone_node) {
-        
-        for(Node* n : node->neighbors) {
-            
-            if(mp.find(n) == mp.end()) {
-                
-                Node* clone = new Node(n->val);
-                mp[n] = clone;
-                clone_node->neighbors.push_back(clone);
-                
-                DFS(n, clone);
-                
-            } else {
-                
-                clone_node->neighbors.push_back(mp[n]);
-                
-            }
-            
-        }
-        
-    }
-    
-    Node* cloneGraph(Node* node) {
-        if(!node)
-            return NULL;
-        
-        mp.clear();
-        
-        //cloned the given node
-        Node* clone_node = new Node(node->val);
-        
-        //Now, cloe its neighbours and recursively their newighbours
-        /*
-                But if a node reappears, then we need to access that cloned node
-                So, store them in a map <Node*, Node*>
-        
-        
-        mp[node] = clone_node;
-        
-        DFS(node, clone_node);
-        
-        return clone_node;
-    }
-};
-
-*/
