81c21000620974177ddb96e860e1442c54e29869
._.jaYanT._. <115420374+iMjayant17@users.noreply.github.com>
2024-11-11 19:10:23 +0530

    Time: 222 ms (88.89%) | Memory: 157.8 MB (61.11%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        map<int,int>mp;
-        unordered_map<int,int>hash;
-        set<int>points;
-        for(auto &it:nums){
-            hash[it]++;
-            mp[it-k]++;
-            mp[it+k+1]--;
-            points.insert(it);
-            points.insert(it-k);
-            points.insert(it+k+1);
+    int maxFrequency(vector<int>& nums, int k, int b) {
+        int maxi = *max_element(nums.begin(),nums.end());
+        
+        vector<int> arr(maxi+5,0);
+        unordered_map<int,int> m;
+        for(auto i:nums){
+            m[i]++;
+            int left = max(i-k,0);
+            int right = min(i+k,maxi+3);
+
+            arr[left]++;
+            arr[right+1]--;
         }
-        int ans=1;
-        int sum=0;
-        for(auto &it:points){
-            sum+=mp[it];
-            ans=max(ans,hash[it]+min(sum-hash[it],numOperations));
+
+        for(int i = 1;i<maxi+4;i++){
+            arr[i]+=arr[i-1];
         }
+        int ans = 0;
+
+        for(int i = 0;i<maxi+4;i++){
+            int val = m[i];
+            val += min(max(0,arr[i]-val) , b);
+            ans = max(ans,val);
+        }
+
         return ans;
     }
 };
