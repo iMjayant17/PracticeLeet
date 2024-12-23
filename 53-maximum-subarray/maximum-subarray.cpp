46e872a040f46db69fbe9c30c8e618acec9fbf18
._.jaYanT._. <115420374+iMjayant17@users.noreply.github.com>
2024-12-24 00:58:24 +0530

    Time: 519 ms (5.31%) | Memory: 71.9 MB (10.82%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans = INT_MIN;
+        int ans = INT_MIN; // kadane algorithm
         int sum = 0;
+        int s = 0,e = 0;
         for(int i = 0;i<nums.size();i++){
             sum+=nums[i];
+            if(sum>ans){
+                ans = sum;
+                e = i;
+            }
             ans = max(ans,sum);
-            if(sum<0) sum = 0;
+            if(sum<0){
+                sum = 0;
+                s = i+1;
+            } 
         }
+
+        for(int i = s;i<=e;i++){
+            cout<<nums[i]<<" ";
+        }
+        cout<<endl;
         return ans;
     }
 };
